beccbb825d59719d08297ecbffc8f4490eaf76a2
Giovani Alessandro Arthur <giovaniarthur69@gmail.com>
2024-09-06 10:09:48 -0300

    inicio teste

---
+++ exercícios/módulo 3/treino/script.js
@@ -0,0 +1,6 @@
+const email = document.getElementById('txte')
+const pass = document.getElementById('txtp')
+function verify() {
+    let name = document.getElementsByTagName("txtn")
+    window.alert(name)
+}
